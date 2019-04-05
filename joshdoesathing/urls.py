d188f555d9f7d6726ece841d565e4eaf95913990
Josh Cook <joshuacook0023@gmail.com>
2019-04-04 21:41:30 -0400

    feat: incorporate blog app

--- joshdoesathing/urls.py
+++ joshdoesathing/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('projects/', include('projects.urls'))
+    path('projects/', include('projects.urls')),
+    path("blog/", include("blogs.urls")),
 ]
