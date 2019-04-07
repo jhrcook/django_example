e80590c5be88bf6723d8f0a64dbd3085d0c72640
Josh Cook <joshuacook0023@gmail.com>
2019-04-07 11:00:41 -0400

    Trying to add projects by admin but running into issues with uploading images and handling static files

--- projects/models.py
+++ projects/models.py
@@ -1,9 +1,12 @@
 from django.db import models
-
+from django.contrib import staticfiles
 # Create your models here.
+
 
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     technology = models.CharField(max_length=20)
-    image = models.FilePathField(path="/img")
+    image = models.ImageField(upload_to="static/projects/img",
+                              default="website_images/Unknown.png")
+    # image = models.FilePathField(path="/projects/img/", default="website_images/Unknown.png")
